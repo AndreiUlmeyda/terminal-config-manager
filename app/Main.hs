c5873a9d2f8d97d4a5611e98cc6ef472e4896aa1
Adrian Schurz <adrian.schurz@check24.de>
2022-05-31 20:31:22 +0200

    Dummy implementation for the main functions, draw a single widget

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,44 @@
 module Main where
 
 import Brick
+import Graphics.Vty.Input.Events
 
-ui :: Widget ()
-ui = str "a"
+data TCMState = TCMState deriving (Show, Eq)
 
-main :: IO ()
-main = simpleMain ui
+main = do
+  initialState <- buildInitialState
+  endState <- defaultMain tcmApp initialState
+  print endState
+
+buildInitialState :: IO TCMState
+buildInitialState = pure TCMState
+
+tcmApp :: App TCMState e ResourceName
+tcmApp =
+  App
+    { appDraw = drawTCM
+    , appChooseCursor = showFirstCursor
+    , appHandleEvent = handleEvent
+    , appStartEvent = pure
+    , appAttrMap = const $ attrMap mempty []
+    }
+
+
+data ResourceName =
+  ResourceName
+  deriving (Show, Eq, Ord)
+
+ui :: Widget ResourceName
+ui = str "Hello, world!"
+
+drawTCM :: TCMState -> [Widget ResourceName]
+drawTCM _ts = [ui]
+
+handleEvent :: TCMState -> BrickEvent n e -> EventM n (Next TCMState)
+handleEvent s e =
+  case e of
+    VtyEvent vtye ->
+      case vtye of
+        EvKey (KChar 'q') [] -> halt s
+        _ -> continue s
+    _ -> continue s
