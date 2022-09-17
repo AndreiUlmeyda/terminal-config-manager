375f82a4ccc8516072ab8f138226f921e1bcb482
Adrian Schurz <AndreiUlmeyda@users.noreply.github.com>
2022-09-17 19:40:44 +0200

    WIP drop dependencies

--- src/Domain/ItemsCursor.hs
+++ src/Domain/ItemsCursor.hs
@@ -1,13 +1,19 @@
 module Domain.ItemsCursor
   ( ItemsCursor (MkItemsCursor),
     makeItemsCursor,
+    cursorUp,
   )
 where
 
 import Infrastructure.Config (ConfigItem)
 
-data ItemsCursor = MkItemsCursor [ConfigItem] deriving stock (Show, Eq)
+type CursorPosition = Int
+
+data ItemsCursor = MkItemsCursor [ConfigItem] CursorPosition deriving stock (Show, Eq)
 
 makeItemsCursor :: [ConfigItem] -> Maybe ItemsCursor
 makeItemsCursor [] = Nothing
-makeItemsCursor items = Just (MkItemsCursor items)
+makeItemsCursor items = Just (MkItemsCursor items 0)
+
+cursorUp :: ItemsCursor -> ItemsCursor
+cursorUp (MkItemsCursor items cp) = MkItemsCursor items (cp + 1)
--- test/Unit/Domain/ItemsCursorSpec.hs
+++ test/Unit/Domain/ItemsCursorSpec.hs
@@ -2,6 +2,7 @@ module Unit.Domain.ItemsCursorSpec where
 
 import Domain.ItemsCursor
   ( ItemsCursor (MkItemsCursor),
+    cursorUp,
     makeItemsCursor,
   )
 import Infrastructure.Config
@@ -21,6 +22,12 @@ spec = do
   describe "Creating a list of items" $ do
     it "given an empty list should result in Nothing" $ do
       makeItemsCursor [] `shouldBe` Nothing
-    it "given an a single item list should succeed" $ do
-      let singleItemList = [MkConfigItem "" "" (MkPattern "") (MkTargetValue "") []]
-       in makeItemsCursor singleItemList `shouldBe` Just (MkItemsCursor singleItemList)
+    it "given a nonempty list should succeed and set the default cursor position to zero" $ do
+      let nonEmptyList = [MkConfigItem "" "" (MkPattern "") (MkTargetValue "") []]
+       in makeItemsCursor nonEmptyList `shouldBe` Just (MkItemsCursor nonEmptyList 0)
+  describe "Moving the cursor" $ do
+    it "upwards should increment the cursor position" $ do
+      let item = MkConfigItem "" "" (MkPattern "") (MkTargetValue "") []
+          nonEmptyList = replicate 2 item
+          (Just itemsCursor) = makeItemsCursor nonEmptyList
+       in cursorUp itemsCursor `shouldBe` MkItemsCursor nonEmptyList 1
