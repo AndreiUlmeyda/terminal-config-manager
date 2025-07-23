ba21826edcc7fa8277fae87b896ef652b698aa3d
Adrian Schurz <adrian.schurz@check24.de>
2025-07-23 15:32:08 +0200

    Use the prebuilt binary during e2e tests

--- test/e2e/test-startup-shutdown.tcl
+++ test/e2e/test-startup-shutdown.tcl
@@ -4,8 +4,8 @@
 exec echo "setting=initial" > /tmp/test-target.conf
 exec echo "config_lines_to_manage:\n  - title: \"Test Setting\"\n    path: \"/tmp/test-target.conf\"\n    pattern: \"setting={{value}}\"\n    targetValue: \"initial\"\n    possibleValues: \[\"initial\", \"changed\", \"final\"\]" > /tmp/test-config.yaml
 
-# run the app
-spawn stack exec terminal-config-manager -- --config /tmp/test-config.yaml
+# run the app using the built binary
+spawn ../../bin/terminal-config-manager --config /tmp/test-config.yaml
 
 set timeout 5
 expect {
